24500c333bb1495542d6dec2e9c57073db0fcc69
Kadir YILMAZ <kadiryilmaz19821@gmail.com>
2026-06-03 01:36:17 +0300

    h1

--- DefenceDB.WebUI/Program.cs
+++ DefenceDB.WebUI/Program.cs
@@ -121,6 +121,7 @@ using (var scope = app.Services.CreateScope())
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
+    app.UseDeveloperExceptionPage(); // Hata tespiti için geçici olarak eklendi
     app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
